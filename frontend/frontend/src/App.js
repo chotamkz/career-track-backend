a28c84cebb3b675a2026d2c8d260a933d1538b98
ShyngysLapidenov <lshyngysa@mail.ru>
2025-03-13 23:59:22 +0600

    protected route

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./Routes";
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from "../services/ProtectedRoute";
 
 const Dashboard = () => <h2>Welcome to the Dashboard</h2>;
 
